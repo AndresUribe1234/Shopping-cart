871a887690d09b16151a7d54bd60f4e65626bd4a
AndresUribe1234 <anduri1997@gmail.com>
2023-02-03 16:08:29 -0500

    cart animation with useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 function HeaderCartButton(props) {
   const cartCtx = useContext(CartContext);
 
+  const [btnAddClasses, setBtnAddClasses] = useState(false);
+
   const numberOfCartItems = cartCtx.items.reduce(
     (currentNumber, ele) => currentNumber + ele.amount,
     0
   );
+
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnAddClasses ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnAddClasses(true);
+
+    const timer = setTimeout(() => {
+      setBtnAddClasses(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onOpenCart}>
+    <button className={btnClasses} onClick={props.onOpenCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
